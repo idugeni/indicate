41fe70350a391767622cde34667a75ff02a4e129
Idugeni <officialelsa21@gmail.com>
2026-09-16 22:54:48 +0700

    Rapatkan badge App Store dan Google Play di mobile

--- src/modules/site/components/network/templates/clean-blue/chrome/store-badges.tsx
+++ src/modules/site/components/network/templates/clean-blue/chrome/store-badges.tsx
@@ -3,29 +3,29 @@ import Image from 'next/image';
 /** Badge toko aplikasi tanpa tautan: visual merespons hover, klik tanpa aksi. */
 export function CleanBlueStoreBadges() {
   return (
-    <p className="m-0 mt-4 grid grid-cols-2 gap-2.5">
+    <p className="m-0 mt-4 flex flex-wrap items-center gap-2.5">
       <span
         title="Segera hadir di App Store"
-        className="cursor-pointer transition duration-180 hover:scale-[1.03] hover:brightness-110"
+        className="flex-none cursor-pointer transition duration-180 hover:scale-[1.03] hover:brightness-110"
       >
         <Image
           src="/brand/app-store.svg"
           alt="Segera hadir di App Store"
           width={120}
           height={40}
-          className="h-10 w-full object-contain object-left"
+          className="h-10 w-32 object-contain object-left"
         />
       </span>
       <span
         title="Segera hadir di Google Play"
-        className="cursor-pointer transition duration-180 hover:scale-[1.03] hover:brightness-110"
+        className="flex-none cursor-pointer transition duration-180 hover:scale-[1.03] hover:brightness-110"
       >
         <Image
           src="/brand/google-play.svg"
           alt="Segera hadir di Google Play"
           width={120}
           height={40}
-          className="h-10 w-full object-contain object-left"
+          className="h-10 w-32 object-contain object-left"
         />
       </span>
     </p>
